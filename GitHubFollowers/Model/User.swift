1b254a9956291231f02e56be77b5230d5d4e4e27
Sedat Barlin <barlinsedat@gmail.com>
2024-05-25 23:58:19 +0300

    Update User.swift

--- GitHubFollowers/Model/User.swift
+++ GitHubFollowers/Model/User.swift
@@ -10,7 +10,7 @@ import Foundation
 struct User: Codable{
     var login: String
     var avatarUrl: String
-    var name: String?
+    var name: String? 
     var location: String? 
     var bio: String?
     var publicRepos: Int
